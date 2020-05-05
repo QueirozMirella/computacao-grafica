18660965bf6119e5b0517f5a43c3986d13e8fef5
QueirozMirella <queiroz.av@gmail.com>
2020-05-05 15:57:06 -0300

    trabalho finalizado com relatorio

--- circulo.c
+++ circulo.c
@@ -2,58 +2,50 @@
 #include <math.h>
 #include <GL/glut.h>
 
-#define PI      3.141592
+#define PI 3.141592
+GLfloat raio = 1;
+GLfloat xcentro = 5;
+GLfloat ycentro = 0;
 
 void Desenha(void){
     glClearColor(1,1,1,0);
     glClear(GL_COLOR_BUFFER_BIT);
-    
     glBegin(GL_POINTS);
-        float t = 0;
-
-        // EIXO X
         glColor3f(0,0,0);
-        for(float x=-20;x<20;x+=0.05){
+        //eixos x e y
+        for(float x=-20;x<20;x+=0.01){
             glVertex3f(x, 0, 0);
         }
-
-        // EIXO Y
-        for(float y=-20;y<20;y+=0.05){
+        for(float y=-20;y<20;y+=0.01){
             glVertex3f(0, y, 0);
         }
-
-        
-        // CIRCUNFERENCIA
-        glColor3f(1,0,0);
-        for(t;t<2*PI;t+=0.0001){
-            glVertex3f(cos(t)+5,sin(t),0);
+        //circulo
+        glColor3f(0,0,1);
+        for(float t=0;t<2*PI;t+=0.01){
+            glVertex3f((raio*cos(t)+xcentro),(raio*sin(t)+ycentro),0);
         }
-
     glEnd();
-
     glFlush();
 }
 
-void Teclado(unsigned char key, int x, int y){
+void Keyboard(unsigned char key, int x, int y){
     if(key == 27)
         exit(0);
 }
 
 void Inicializa(void){
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(-20.0,20.0,-20.0,20.0);
+    gluOrtho2D(-20,20,-20,20);
     glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char *argv[]){
-
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500,500);
     glutCreateWindow("Trabalho P1");
-
     glutDisplayFunc(Desenha);
-    glutKeyboardFunc(Teclado);
+    glutKeyboardFunc(Keyboard);
     Inicializa();
     glutMainLoop();
 
